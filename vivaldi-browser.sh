c0c3f91fab11e469f670c91f041c02af48ff6bce
Amaury Libert <amaury-libert@hotmail.com>
2022-08-12 12:55:24 +0200

    Add files via upload

---
+++ vivaldi-browser.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Autheur:
+#   Amaury Libert <amaury-libert@hotmail.com> de Blabla Linux <https://blablalinux.be>
+#
+# Description:
+#   Installation automatisée du navigateur Vivaldi à partir du dépôt tiers de ce dernier. Script valable pour Debian, Ubuntu, Mint...
+#   Automated installation of the Vivaldi browser from its third-party repository. Script valid for Debian, Ubuntu, Mint...
+#
+# Préambule Légal:
+# 	Ce script est un logiciel libre.
+# 	Vous pouvez le redistribuer et / ou le modifier selon les termes de la licence publique générale GNU telle que publiée par la Free Software Foundation; version 3.
+#
+# 	Ce script est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE; sans même la garantie implicite de QUALITÉ MARCHANDE ou d'ADÉQUATION À UN USAGE PARTICULIER.
+# 	Voir la licence publique générale GNU pour plus de détails.
+#
+# 	Licence publique générale GNU : <https://www.gnu.org/licenses/gpl-2.0.txt>
+#
+echo "Effacement écran..."
+clear
+#
+echo "Installation paquets requis"
+apt install apt-transport-https wget
+#
+echo "Importation clé publique (/usr/share/keyrings/vivaldi-browser.gpg)"
+wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | gpg --dearmor | dd of=/usr/share/keyrings/vivaldi-browser.gpg
+#
+echo "Génération sources.list (/etc/apt/sources.list.d/vivaldi-archive.list)"
+echo "deb [signed-by=/usr/share/keyrings/vivaldi-browser.gpg arch=amd64] https://repo.vivaldi.com/archive/deb/ stable main" > /etc/apt/sources.list.d/vivaldi-archive.list
+#
+echo "Raffraichissement dépôts"
+apt update
+#
+echo "Installation Vivaldi"
+apt install vivaldi
